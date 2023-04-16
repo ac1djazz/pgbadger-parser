6c472379def22e79d3f8a7dc94f848d36e682e11
Denis Vasiliev <denis.vasiliev@buyk.com>
2023-04-16 22:25:46 +0300

    Improve script

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-This is scipt fot parsing daily postgresql log. 
-Give files with hour's data 
+# PostgreSQL Log Analyzer
+
+This script helps you analyze and archive PostgreSQL logs by generating hourly and daily HTML reports using `pgbadger`. The script first syncs log files from a remote server to a local folder, then generates reports for each hour of the previous day, and finally creates an overall daily report. The log files are archived and the reports are moved to a web-accessible folder.
+
+## Requirements
+
+- `rsync` for syncing log files from the remote server
+- `pgbadger` for log analysis and report generation
+- `tar` for archiving log files
+
+## Configuration
+
+Before using the script, you need to set the following variables at the beginning of the script:
+
+- `user`: The username for connecting to the remote PostgreSQL server
+- `ip`: The IP address of the remote PostgreSQL server
+- `log_folder`: The path to the folder containing PostgreSQL logs on the remote server
+- `parser_folder`: The path to the local folder where logs will be synced and reports will be generated
+
+The folder structure should look like this:
+
+parser_folder
+├── logs
+└── web
+
+Make the script executable with the following command:
+
+```bash
+chmod +x parser.sh
+
+
+Scheduling
+
+crontab -e
+
+10 0 * * * /path/to/parser.sh
--- parser.sh
+++ parser.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 yesterday=$(date -d "1 day ago" '+%Y-%m-%d')
+user="<user>"
+ip="<IP_POSTGRES_SERVER>"
+log_folder="<FOLDER_PG_LOG>"
+parser_folder="<PARSER_FOLDER>"
+local_logs="$parser_folder/logs"
+web_logs="$parser_folder/web/$yesterday"
+
 # Take logs files from server
-rsync -avz <user>@<IP POSTGRES SERVER>:<FOLDER PG_LOG>/*$yesterday*.log <PARSER_FOLDER>/logs/
-mkdir <PARSER_FOLDER>/web/$yesterday
-cd <PARSER_FOLDER>/logs/
-pgbadger -j 16 -t 100 *.log -o $yesterday-all.html
-pgbadger -j 16 -t 100 -b "$yesterday 00:00:00" -e "$yesterday 01:00:00"  *.log -o $yesterday-00-00_01-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 01:00:00" -e "$yesterday 02:00:00"  *.log -o $yesterday-01-00_02-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 02:00:00" -e "$yesterday 03:00:00"  *.log -o $yesterday-02-00_03-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 03:00:00" -e "$yesterday 04:00:00"  *.log -o $yesterday-03-00_04-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 04:00:00" -e "$yesterday 05:00:00"  *.log -o $yesterday-04-00_05-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 05:00:00" -e "$yesterday 06:00:00"  *.log -o $yesterday-05-00_06-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 06:00:00" -e "$yesterday 07:00:00"  *.log -o $yesterday-06-00_07-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 07:00:00" -e "$yesterday 08:00:00"  *.log -o $yesterday-07-00_08-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 08:00:00" -e "$yesterday 09:00:00"  *.log -o $yesterday-08-00_09-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 09:00:00" -e "$yesterday 10:00:00"  *.log -o $yesterday-09-00_10-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 10:00:00" -e "$yesterday 11:00:00"  *.log -o $yesterday-10-00_11-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 11:00:00" -e "$yesterday 12:00:00"  *.log -o $yesterday-11-00_12-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 12:00:00" -e "$yesterday 13:00:00"  *.log -o $yesterday-12-00_13-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 13:00:00" -e "$yesterday 14:00:00"  *.log -o $yesterday-13-00_14-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 14:00:00" -e "$yesterday 15:00:00"  *.log -o $yesterday-14-00_15-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 15:00:00" -e "$yesterday 16:00:00"  *.log -o $yesterday-15-00_16-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 16:00:00" -e "$yesterday 17:00:00"  *.log -o $yesterday-16-00_17-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 17:00:00" -e "$yesterday 18:00:00"  *.log -o $yesterday-17-00_18-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 18:00:00" -e "$yesterday 19:00:00"  *.log -o $yesterday-18-00_19-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 19:00:00" -e "$yesterday 20:00:00"  *.log -o $yesterday-19-00_20-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 20:00:00" -e "$yesterday 21:00:00"  *.log -o $yesterday-20-00_21-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 21:00:00" -e "$yesterday 22:00:00"  *.log -o $yesterday-21-00_22-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 22:00:00" -e "$yesterday 23:00:00"  *.log -o $yesterday-22-00_23-00.html
-pgbadger -j 16 -t 100 -b "$yesterday 23:00:00" -e "$yesterday 23:59:59"  *.log -o $yesterday-23-00_24-00.html
-# Archive log files 
-tar cvfz posgresql-$yesterday-log.tar.gz *.log
+rsync -avz ${user}@${ip}:${log_folder}/*${yesterday}*.log ${local_logs}/
+mkdir ${web_logs}
+cd ${local_logs}
+
+# Generate reports for each hour
+for i in {0..23}; do
+    start_hour=$(printf "%02d" $i)
+    end_hour=$(printf "%02d" $((i+1)))
+    start_time="${yesterday} ${start_hour}:00:00"
+    end_time="${yesterday} ${end_hour}:00:00"
+    output_file="${yesterday}-${start_hour}-00_${end_hour}-00.html"
+
+    pgbadger -j 16 -t 100 -b "${start_time}" -e "${end_time}" *.log -o ${output_file}
+done
+
+# Generate an overall report
+pgbadger -j 16 -t 100 *.log -o ${yesterday}-all.html
+
+# Archive log files
+tar cvfz posgresql-${yesterday}-log.tar.gz *.log
 rm *.log
-cp * /data/postgres_logs/web/$yesterday
-rm -rf *
+
+# Move report files to web directory
+mv * ${web_logs}
